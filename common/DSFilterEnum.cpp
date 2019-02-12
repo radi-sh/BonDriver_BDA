c8dceee498766b512ca7f2ebda80dcd6afa71f61
radi_sh <masamory7@gmail.com>
2019-02-12 21:17:04 +0900

    [CDSFilterEnum] class CDSEnumPins と class CDSEnumNodes を追加

--- common/DSFilterEnum.cpp
+++ common/DSFilterEnum.cpp
@@ -11,8 +11,7 @@
 
 #include <DShow.h>
 
-#include "Rpc.h"
-#pragma comment(lib, "Rpcrt4.lib")
+#pragma comment(lib, "strmiids")
 
 CDSFilterEnum::CDSFilterEnum(CLSID clsid)
 	: CDSFilterEnum(clsid, 0)
@@ -25,15 +24,13 @@ CDSFilterEnum::CDSFilterEnum(CLSID clsid, DWORD dwFlags)
 
 	if (FAILED(hr = m_pICreateDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC))) {
 		std::wstring e(L"CDSFilterEnum: Error in CoCreateInstance.");
-		throw e;
-		return; /* not reached */
+		return;
 	}
 
 	if ((FAILED(hr = m_pICreateDevEnum->CreateClassEnumerator(clsid, &m_pIEnumMoniker, dwFlags))) || (hr != S_OK)) {
 		// CreateClassEnumerator が作れない || 見つからない
 		std::wstring e(L"CDSFilterEnum: Error in CreateClassEnumerator.");
-		throw e;
-		return; /* not reached */
+		return;
 	}
 
 	return;
@@ -45,6 +42,10 @@ CDSFilterEnum::~CDSFilterEnum(void)
 
 HRESULT CDSFilterEnum::next(void)
 {
+	if (!m_pIEnumMoniker) {
+		return S_FALSE;
+	}
+
 	m_pIMoniker.Release();
 
 	HRESULT hr = m_pIEnumMoniker->Next(1, &m_pIMoniker, 0);
@@ -101,7 +102,7 @@ HRESULT CDSFilterEnum::getFriendlyName(std::wstring * pName)
     CComPtr<IPropertyBag> pBag;
 	HRESULT hr;
 
-	if(FAILED(hr = m_pIMoniker->BindToStorage(NULL, NULL, IID_IPropertyBag, (void**)&pBag))) {
+	if(FAILED(hr = m_pIMoniker->BindToStorage(NULL, NULL, IID_IPropertyBag, (void **)&pBag))) {
 		OutputDebug(L"CDSFilterEnum::getFriendlyName: IMoniker::BindToStorage method failed.\n");
         return hr;
     }
@@ -157,28 +158,177 @@ std::wstring CDSFilterEnum::getRegistryName(IBaseFilter * pFilter)
 	if (pIpersist) {
 		CLSID clsidFilterObj;
 		if (SUCCEEDED(hr = pIpersist->GetClassID(&clsidFilterObj))) {
-			// CLSIDを文字列に変換
-			RPC_STATUS rpcret;
-			WCHAR *wszUuid = NULL;
-			if ((rpcret = ::UuidToStringW(&clsidFilterObj, (RPC_WSTR *)&wszUuid)) == RPC_S_OK) {
-				std::wstring strRegKey(wszUuid);
-				::RpcStringFreeW((RPC_WSTR *)&wszUuid);
-				// レジストリキー名を作成
-				strRegKey = L"CLSID\\{" + strRegKey + L"}";
-				// レジストリから名称を取得
-				LSTATUS ret;
-				HKEY hk;
-				if ((ret = ::RegOpenKeyExW(HKEY_CLASSES_ROOT, strRegKey.c_str(), 0, KEY_READ, &hk)) == ERROR_SUCCESS) {
-					WCHAR data[128];
-					DWORD size = sizeof(data) / sizeof(data[0]);
-					if ((ret = ::RegQueryValueExW(hk, NULL, NULL, NULL, (BYTE *)data, &size)) == ERROR_SUCCESS) {
-						::RegCloseKey(hk);
-						return std::wstring(data);
-					}
+			// CLSIDからレジストリキー名を作成
+			std::wstring strRegKey(L"CLSID\\{" + common::GUIDToWString(clsidFilterObj) + L"}");
+			// レジストリから名称を取得
+			LSTATUS ret;
+			HKEY hk;
+			if ((ret = ::RegOpenKeyExW(HKEY_CLASSES_ROOT, strRegKey.c_str(), 0, KEY_READ, &hk)) == ERROR_SUCCESS) {
+				WCHAR data[128];
+				DWORD size = sizeof(data) / sizeof(data[0]);
+				if ((ret = ::RegQueryValueExW(hk, NULL, NULL, NULL, (BYTE *)data, &size)) == ERROR_SUCCESS) {
 					::RegCloseKey(hk);
+					return std::wstring(data);
 				}
+				::RegCloseKey(hk);
 			}
 		}
 	}
 	return L"Unknown filter";
 }
+
+CDSEnumPins::CDSEnumPins(IBaseFilter * pFilter)
+{
+	HRESULT hr;
+	if (FAILED(hr = pFilter->EnumPins(&m_pIEnumPins))) {
+		OutputDebug(L"CDSEnumPins: Error in EnumPins.");
+	}
+
+	return;
+}
+
+CDSEnumPins::~CDSEnumPins(void)
+{
+}
+
+HRESULT CDSEnumPins::getNextPin(IPin ** ppIPin)
+{
+	PIN_DIRECTION dir;
+	return getNextPin(ppIPin, &dir);
+}
+
+HRESULT CDSEnumPins::getNextPin(IPin ** ppIPin, PIN_DIRECTION * pinDir)
+{
+	if (!m_pIEnumPins) {
+		return S_FALSE;
+	}
+
+	HRESULT hr;
+	if (S_OK != (hr = m_pIEnumPins->Next(1, ppIPin, NULL))) {
+		return hr;
+	}
+
+	if (FAILED(hr = (*ppIPin)->QueryDirection(pinDir))) {
+		OutputDebug(L"Can not get PinDir.");
+		return hr;
+	}
+	return S_OK;
+}
+
+HRESULT CDSEnumPins::getNextPin(IPin ** ppIPin, PIN_DIRECTION pinDir)
+{
+	HRESULT hr;
+	while (1) {
+		CComPtr<IPin> pPin;
+		PIN_DIRECTION dir;
+		if (S_OK != (hr = getNextPin(&pPin, &dir))) {
+			break;
+		}
+
+		if (pinDir == dir) {
+			*ppIPin = pPin;
+			(*ppIPin)->AddRef();
+			break;
+		}
+	}
+
+	return hr;
+}
+
+HRESULT CDSEnumPins::Reset(void)
+{
+	if (!m_pIEnumPins) {
+		return S_OK;
+	}
+
+	return m_pIEnumPins->Reset();
+}
+
+CDSEnumNodes::CDSEnumNodes(IBaseFilter * pFilter)
+	: m_pIBDA_Topology(pFilter),
+	m_nNodeTypes(0),
+	m_nNodeType()
+{
+	if (!m_pIBDA_Topology) {
+		OutputDebug(L"CDSEnumNodes: Fail to get IBDA_Topology interface.\n");
+		return;
+	}
+
+	HRESULT hr;
+
+	if (FAILED(hr = m_pIBDA_Topology->GetNodeTypes(&m_nNodeTypes, MAX_NODES, m_nNodeType))) {
+		OutputDebug(L"CDSEnumNodes: Fail to get NodeTypes.\n");
+		return;
+	}
+
+	for (ULONG i = 0; i < m_nNodeTypes; i++) {
+		ULONG Interfaces;
+		GUID Interface[MAX_NODE_INTERFACES];
+		if (FAILED(m_pIBDA_Topology->GetNodeInterfaces(m_nNodeType[i], &Interfaces, MAX_NODE_INTERFACES, Interface))) {
+			OutputDebug(L"CDSEnumNodes: Fail to get GetNodeInterfaces for NodeType[%ld]=%ld.\n", i, m_nNodeType[i]);
+		}
+		else {
+			for (ULONG j = 0; j < Interfaces; j++) {
+				std::wstring sGuid(common::GUIDToWString(Interface[j]));
+				OutputDebug(L"CDSEnumNodes: Found GUID=%s, NodeType[%ld]=%ld.\n", sGuid.c_str(), i, m_nNodeType[i]);
+				m_Interfaces.emplace(sGuid, i);
+			}
+		}
+	}
+}
+
+CDSEnumNodes::~CDSEnumNodes(void)
+{
+	m_Interfaces.clear();
+}
+
+ULONG CDSEnumNodes::getCount(void)
+{
+	return m_nNodeTypes;
+}
+
+ULONG CDSEnumNodes::getNodeType(ULONG index)
+{
+	return m_nNodeType[index];
+}
+
+HRESULT CDSEnumNodes::getControlNode(ULONG nodeType, IUnknown ** ppControlNode)
+{
+	if (!m_pIBDA_Topology) {
+		return E_POINTER;
+	}
+
+	return m_pIBDA_Topology->GetControlNode(0UL, 1UL, nodeType, ppControlNode);
+}
+
+HRESULT CDSEnumNodes::getControlNode(GUID guidInterface, IUnknown ** ppControlNode)
+{
+	return getControlNodeFromNodeType(guidInterface, ppControlNode, (ULONG)-1);
+}
+
+HRESULT CDSEnumNodes::getControlNodeFromNodeType(GUID guidInterface, IUnknown ** ppControlNode, ULONG nodeType)
+{
+	return getControlNode(findControlNodeFromNodeType(guidInterface, nodeType), ppControlNode);
+}
+
+ULONG CDSEnumNodes::findControlNode(GUID guidInterface)
+{
+	return findControlNodeFromNodeType(guidInterface, (ULONG)-1);
+}
+
+ULONG CDSEnumNodes::findControlNodeFromNodeType(GUID guidInterface, ULONG nodeType)
+{
+	if (!m_pIBDA_Topology) {
+		return (ULONG)-1;
+	}
+
+	auto range = m_Interfaces.equal_range(common::GUIDToWString(guidInterface));
+	for (auto it = range.first; it != range.second; it++) {
+		if (m_nNodeType[it->second] == nodeType || nodeType == (ULONG)-1) {
+			return m_nNodeType[it->second];
+		}
+	}
+	// not found
+	return (ULONG)-1;
+}
+
--- common/DSFilterEnum.h
+++ common/DSFilterEnum.h
@@ -6,8 +6,10 @@
 #pragma once
 
 #include <dshow.h>
+#include <bdaiface.h>
 
 #include <string>
+#include <map>
 #include <atlbase.h>
 
 class CDSFilterEnum
@@ -30,4 +32,42 @@ private:
 	CComPtr<IEnumMoniker> m_pIEnumMoniker;
 	CComPtr<ICreateDevEnum> m_pICreateDevEnum;
 	CComPtr<IMoniker> m_pIMoniker;
+};
+
+class CDSEnumPins
+{
+public:
+	CDSEnumPins(IBaseFilter * pFilter);
+	virtual ~CDSEnumPins(void);
+
+	HRESULT getNextPin(IPin ** ppIPin);
+	HRESULT getNextPin(IPin ** ppIPin, PIN_DIRECTION * pinDir);
+	HRESULT getNextPin(IPin ** ppIPin, PIN_DIRECTION pinDir);
+	HRESULT Reset(void);
+
+private:
+	CComPtr<IEnumPins> m_pIEnumPins;
+};
+
+class CDSEnumNodes
+{
+public:
+	CDSEnumNodes(IBaseFilter * pFilter);
+	virtual ~CDSEnumNodes(void);
+
+	ULONG getCount(void);
+	ULONG getNodeType(ULONG index);
+	HRESULT getControlNode(ULONG nodeType, IUnknown ** ppControlNode);
+	HRESULT getControlNode(GUID guidInterface, IUnknown ** ppControlNode);
+	HRESULT getControlNodeFromNodeType(GUID guidInterface, IUnknown ** ppControlNode, ULONG nodeType);
+	ULONG findControlNode(GUID guidInterface);
+	ULONG findControlNodeFromNodeType(GUID guidInterface, ULONG nodeType);
+
+private:
+	CComQIPtr<IBDA_Topology> m_pIBDA_Topology;
+	static constexpr size_t MAX_NODES = 32;
+	static constexpr size_t MAX_NODE_INTERFACES = 32;
+	ULONG m_nNodeTypes;
+	ULONG m_nNodeType[MAX_NODES];
+	std::multimap<std::wstring, ULONG> m_Interfaces;
 };
