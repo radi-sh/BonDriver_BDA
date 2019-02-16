711d3809b0d333ea68190a5c385af819e74bc0ff
radi_sh <masamory7@gmail.com>
2019-02-16 10:22:33 +0900

    ソースコードのインデント位置修正

--- BdaSpecials/DVBWorld/DVBWorldSpecials.cpp
+++ BdaSpecials/DVBWorld/DVBWorldSpecials.cpp
@@ -44,7 +44,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	case DLL_PROCESS_DETACH:
 		break;
 	}
-		return TRUE;
+	return TRUE;
 }
 
 // CreateBdaSpecials(void) method
