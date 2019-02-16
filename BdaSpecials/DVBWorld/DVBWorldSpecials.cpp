f07574332f2ac2f9bd6f2afad4b3958e4281af6e
radi_sh <masamory7@gmail.com>
2019-02-16 10:57:43 +0900

    [DVBWorld] Debug ビルド時にメモリリーク検出を行うようにした

--- BdaSpecials/DVBWorld/DVBWorldSpecials.cpp
+++ BdaSpecials/DVBWorld/DVBWorldSpecials.cpp
@@ -3,6 +3,12 @@
 //   Implementation of CDVBWorldSpecials class
 //------------------------------------------------------------------------------
 
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
 #include "common.h"
 
 #include "DVBWorldSpecials.h"
@@ -37,6 +43,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
+#ifdef _DEBUG
+		::_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 		// モジュールハンドル保存
 		hMySelf = hModule;
 		break;
