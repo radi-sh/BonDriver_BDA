a6a36e6963f3c8ef795955f7ff0db9c7223433e5
radi_sh <masamory7@gmail.com>
2019-08-14 12:42:05 +0900

    [TBS] いくつかの変更と修正
    ・Tuner input pin の IKsPropertySet が取得できていなかった不具合を修正
    ・NULL pointer チェック漏れを修正
    ・DebugLog をファイルに書き出すようにした
    ・ LNBPowerON / Set22KHz / SetTSID のオプションを ini ファイルで指定できるようにした
    ・ ヘッダファイルの整理

--- BdaSpecials/TBS/TBSSpecials.cpp
+++ BdaSpecials/TBS/TBSSpecials.cpp
@@ -18,6 +18,7 @@
 
 #include <dshow.h>
 
+#include "CIniFileAccess.h"
 #include "DSFilterEnum.h"
 
 FILE *g_fpLog = NULL;
@@ -42,6 +43,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID /*lpRese
 		break;
 
 	case DLL_PROCESS_DETACH:
+		// デバッグログファイルのクローズ
+		CloseDebugLog();
 		break;
 	}
 	return TRUE;
@@ -54,11 +57,29 @@ __declspec(dllexport) IBdaSpecials* CreateBdaSpecials(CComPtr<IBaseFilter> pTune
 	return new CTBSSpecials(pTunerDevice);
 }
 
+__declspec(dllexport) HRESULT CheckAndInitTuner(IBaseFilter* /*pTunerDevice*/, const WCHAR* /*szDisplayName*/, const WCHAR* /*szFriendlyName*/, const WCHAR* szIniFilePath)
+{
+	CIniFileAccess IniFileAccess(szIniFilePath);
+
+	// DebugLogを記録するかどうか
+	if (IniFileAccess.ReadKeyB(L"TBS", L"DebugLog", FALSE)) {
+		// INIファイルのファイル名取得
+		// DebugLogのファイル名取得
+		SetDebugLog(common::GetModuleName(CTBSSpecials::m_hMySelf) + L"log");
+	}
+
+	return S_OK;
+}
+
 // Constructor
 /////////////////////////////////////
 
 CTBSSpecials::CTBSSpecials(CComPtr<IBaseFilter> pTunerDevice)
-	: m_pTunerDevice(pTunerDevice)
+	: m_pTunerDevice(pTunerDevice),
+	m_pPropsetTunerPin(NULL),
+	m_bLNBPowerON(FALSE),
+	m_bSet22KHz(FALSE),
+	m_bSetTSID(FALSE)
 {
 	if (m_pTunerDevice) {
 		CDSEnumPins DSEnumPins(m_pTunerDevice);
@@ -68,7 +89,7 @@ CTBSSpecials::CTBSSpecials(CComPtr<IBaseFilter> pTunerDevice)
 				break;
 			}
 			CComQIPtr<IKsPropertySet> pPropsetTunerPin(pPin);
-			if (!pPropsetTunerPin) {
+			if (pPropsetTunerPin) {
 				m_pPropsetTunerPin = pPropsetTunerPin;
 				break;
 			}
@@ -95,23 +116,18 @@ CTBSSpecials::~CTBSSpecials()
 /////////////////////////////////////
 const HRESULT CTBSSpecials::InitializeHook(void)
 {
-	if (m_pTunerDevice == NULL) {
+	if (!m_pTunerDevice) {
+		OutputDebug(L"Invalid tuner divice pointer.\n");
 		return E_POINTER;
 	}
 
 	if (!m_pPropsetTunerPin) {
-		return E_NOINTERFACE;
+		OutputDebug(L"Invalid IKsPropertySet pointer.\n");
+		return E_POINTER;
 	}
 		
-	HRESULT hr;
-	DWORD TypeSupport = 0;
-	if ((hr = m_pPropsetTunerPin->QuerySupported(KSPROPSETID_BdaTunerExtensionProperties, KSPROPERTY_BDA_DISEQC_MESSAGE, &TypeSupport)) != S_OK) {
-		return E_NOINTERFACE;
-	}
-
-	hr = SetLNBPower(TRUE);
-	if (hr != S_OK) {
-		OutputDebug(L"SetLNBPower failed.\n");
+	if (m_bLNBPowerON) {
+		SetLNBPower(TRUE);
 	}
 
 	return S_OK;
@@ -124,14 +140,29 @@ const HRESULT CTBSSpecials::Set22KHz(bool bActive)
 
 const HRESULT CTBSSpecials::Set22KHz(long nTone)
 {
+	if (!m_bSet22KHz) {
+		return E_NOINTERFACE;
+	}
+
+	if (!m_pPropsetTunerPin) {
+		return E_POINTER;
+	}
+
+	HRESULT hr;
+	DWORD TypeSupport = 0;
+	if ((hr = m_pPropsetTunerPin->QuerySupported(KSPROPSETID_BdaTunerExtensionProperties, KSPROPERTY_BDA_DISEQC_MESSAGE, &TypeSupport)) != S_OK) {
+		OutputDebug(L"Set22KHz: KSPROPERTY_BDA_DISEQC_MESSAGE is not supported.\n");
+		m_bSet22KHz = FALSE;
+		return E_NOINTERFACE;
+	}
+
 	DWORD bytesReturned;
 	DISEQC_MESSAGE_PARAMS DiSEqCRequest;
 	ZeroMemory(&DiSEqCRequest, sizeof(DiSEqCRequest));
 	DiSEqCRequest.tbscmd_mode = TBSDVBSCMD_22KTONEDATA;    
-	DiSEqCRequest.Tone_Data_Burst = Value_Burst_OFF;
+	DiSEqCRequest.Tone_Data_Burst = Tone_Data_Disable;
 	DiSEqCRequest.HZ_22K = !!nTone ? HZ_22K_ON : HZ_22K_OFF;
 
-	HRESULT hr;
 	hr = m_pPropsetTunerPin->Get(KSPROPSETID_BdaTunerExtensionProperties,
 		KSPROPERTY_BDA_DISEQC_MESSAGE, 
 		&DiSEqCRequest,sizeof(DISEQC_MESSAGE_PARAMS),
@@ -139,36 +170,55 @@ const HRESULT CTBSSpecials::Set22KHz(long nTone)
 		&bytesReturned);
 
 	return hr;
-
 }
 
-const HRESULT CTBSSpecials::SetTSid(long TSID) //add 20190715 by Davin zhang TBS
+const HRESULT CTBSSpecials::SetTSid(long TSID) // add 20190715 by Davin zhang TBS
 {
-	DWORD TypeSupport = 0;
-	HRESULT hr;
-
-	hr = m_pPropsetTunerPin->QuerySupported(KSPROPSETID_BdaTunerExtensionProperties,
-		KSPROPERTY_BDA_SETTSID, &TypeSupport);
-	if FAILED(hr)
+	if (!m_bSetTSID) {
 		return E_NOINTERFACE;
+	}
+
+	if (!m_pPropsetTunerPin) {
+		return E_POINTER;
+	}
+
+	HRESULT hr;
+	DWORD TypeSupport = 0;
+	if ((hr = m_pPropsetTunerPin->QuerySupported(KSPROPSETID_BdaTunerExtensionProperties, KSPROPERTY_BDA_SETTSID, &TypeSupport)) != S_OK) {
+		OutputDebug(L"SetTSID: KSPROPERTY_BDA_SETTSID is not supported.\n");
+		m_bSetTSID = FALSE;
+		return E_NOINTERFACE;
+	}
 
 	hr = m_pPropsetTunerPin->Set(KSPROPSETID_BdaTunerExtensionProperties,
 		KSPROPERTY_BDA_SETTSID,
 		&TSID, sizeof(long),
 		&TSID, sizeof(long));
-	if FAILED(hr)
-		return E_NOTIMPL;
+
+	return hr;
+}
+
+const HRESULT CTBSSpecials::ReadIniFile(const WCHAR* szIniFilePath)
+{
+	CIniFileAccess IniFileAccess(szIniFilePath);
+	IniFileAccess.SetSectionName(L"TBS");
+
+	// 固有DllでLNB Power ONを行う
+	m_bLNBPowerON = IniFileAccess.ReadKeyB(L"LNBPowerON", m_bLNBPowerON);
+
+	// 固有Dllでトーン制御を行う
+	m_bSet22KHz = IniFileAccess.ReadKeyB(L"Set22KHz", m_bSet22KHz);
+
+	// 固有DllでTSIDをセットする
+	m_bSetTSID = IniFileAccess.ReadKeyB(L"SetTSID", m_bSetTSID);
 
 	return S_OK;
-
 }
 
 const HRESULT CTBSSpecials::FinalizeHook(void)
 {
-
-	HRESULT hr = SetLNBPower(FALSE);
-	if (hr != S_OK) {
-		OutputDebug(L"SetLNBPower failed.\n");
+	if (m_bLNBPowerON) {
+		SetLNBPower(FALSE);
 	}
 
 	return S_OK;
@@ -176,13 +226,28 @@ const HRESULT CTBSSpecials::FinalizeHook(void)
 
 const HRESULT CTBSSpecials::SetLNBPower(bool bActive)
 {
+	if (!m_bLNBPowerON) {
+		return E_NOINTERFACE;
+	}
+
+	if (!m_pPropsetTunerPin) {
+		return E_POINTER;
+	}
+
+	HRESULT hr;
+	DWORD TypeSupport = 0;
+	if ((hr = m_pPropsetTunerPin->QuerySupported(KSPROPSETID_BdaTunerExtensionProperties, KSPROPERTY_BDA_DISEQC_MESSAGE, &TypeSupport)) != S_OK) {
+		OutputDebug(L"SetLNBPower: KSPROPERTY_BDA_DISEQC_MESSAGE is not supported.\n");
+		m_bLNBPowerON = FALSE;
+		return E_NOINTERFACE;
+	}
+
 	DWORD bytesReturned;
 	DISEQC_MESSAGE_PARAMS DiSEqCRequest;
 	ZeroMemory(&DiSEqCRequest, sizeof(DiSEqCRequest));
 	DiSEqCRequest.tbscmd_mode = TBSDVBSCMD_LNBPOWER;
 	DiSEqCRequest.b_LNBPower = bActive? LNB_POWER_ON : LNB_POWER_OFF;
 
-	HRESULT hr;
 	hr = m_pPropsetTunerPin->Get(KSPROPSETID_BdaTunerExtensionProperties,
 		KSPROPERTY_BDA_DISEQC_MESSAGE,
 		&DiSEqCRequest,sizeof(DISEQC_MESSAGE_PARAMS),
--- BdaSpecials/TBS/TBSSpecials.h
+++ BdaSpecials/TBS/TBSSpecials.h
@@ -7,10 +7,6 @@
 
 // GUID/Structure/constant for tuning
 
-// this is defined in bda tuner/demod driver source (splmedia.h)
-const GUID KSPROPSETID_BdaTunerExtensionProperties =
-{0xfaa8f3e5, 0x31d4, 0x4e41, {0x88, 0xef, 0xd9, 0xeb, 0x71, 0x6f, 0x6e, 0xc9}};
-
 class CTBSSpecials : public IBdaSpecials2b5
 {
 public:
@@ -24,6 +20,7 @@ public:
 
 	const HRESULT Set22KHz(long nTone);
 	const HRESULT SetTSid(long TSID); //add 20190715 by Davin zhang TBS
+	const HRESULT ReadIniFile(const WCHAR* szIniFilePath);
 
 	virtual void Release(void);
 
@@ -32,83 +29,93 @@ public:
 private:
 	CComPtr<IKsPropertySet> m_pPropsetTunerPin;
 	CComPtr<IBaseFilter> m_pTunerDevice;
+	BOOL m_bLNBPowerON;									// 固有DllでLNB Power ONを行う
+	BOOL m_bSet22KHz;									// 固有Dllでトーン制御を行う
+	BOOL m_bSetTSID;									// 固有DllでTSIDをセットする
 
 	////////////////////////////////////////////////////
 	// Definitions for TBS
 	////////////////////////////////////////////////////
-	static constexpr UCHAR HZ_22K_OFF = 0;
-	static constexpr UCHAR HZ_22K_ON = 1;
-	static constexpr UCHAR LNB_POWER_OFF = 0;
-	static constexpr UCHAR LNB_POWER_ON = 1;
 
-	static constexpr UCHAR Value_Data_Burst_ON = 1;
-	static constexpr UCHAR Value_Tone_Burst_ON = 0;
-	static constexpr UCHAR Value_Burst_OFF = 2;
-
-	static constexpr BYTE DISEQC_TX_BUFFER_SIZE = 150; // 3 bytes per message * 50 messages
-	static constexpr BYTE DISEQC_RX_BUFFER_SIZE = 8;   // reply fifo size, hardware limitation
-
-	typedef enum _TBSDVBSExtensionPropertiesCMDMode {
-		TBSDVBSCMD_LNBPOWER=0x00,
-		TBSDVBSCMD_MOTOR=0x01,
-		TBSDVBSCMD_22KTONEDATA=0x02,
-		TBSDVBSCMD_DISEQC=0x03
-	} TBSDVBSExtensionPropertiesCMDMode;
+	static constexpr BYTE DISEQC_TX_BUFFER_SIZE = 150;	// 3 bytes per message * 50 messages
+	static constexpr BYTE DISEQC_RX_BUFFER_SIZE = 8;	// reply fifo size, hardware limitation
 
 	// this is defined in bda tuner/demod driver source (splmedia.h)
-	typedef enum {
-		KSPROPERTY_BDA_DISEQC_MESSAGE = 0,  //Custom property for Diseqc messaging
-		KSPROPERTY_BDA_DISEQC_INIT,         //Custom property for Intializing Diseqc.
-		KSPROPERTY_BDA_SCAN_FREQ,           //Not supported 
-		KSPROPERTY_BDA_CHANNEL_CHANGE,      //Custom property for changing channel
-		KSPROPERTY_BDA_DEMOD_INFO,          //Custom property for returning demod FW state and version
-		KSPROPERTY_BDA_EFFECTIVE_FREQ,      //Not supported 
-		KSPROPERTY_BDA_SIGNAL_STATUS,       //Custom property for returning signal quality, strength, BER and other attributes
-		KSPROPERTY_BDA_LOCK_STATUS,         //Custom property for returning demod lock indicators 
-		KSPROPERTY_BDA_ERROR_CONTROL,       //Custom property for controlling error correction and BER window
-		KSPROPERTY_BDA_CHANNEL_INFO,        //Custom property for exposing the locked values of frequency,symbol rate etc after
-                                     		//corrections and adjustments
+	static constexpr GUID KSPROPSETID_BdaTunerExtensionProperties = { 0xfaa8f3e5, 0x31d4, 0x4e41, {0x88, 0xef, 0xd9, 0xeb, 0x71, 0x6f, 0x6e, 0xc9} };
+
+	// this is defined in bda tuner/demod driver source (splmedia.h)
+	enum KSPROPERTY_BDA_TUNER_EXTENSION : ULONG {
+		KSPROPERTY_BDA_DISEQC_MESSAGE = 0,  // Custom property for Diseqc messaging
+		KSPROPERTY_BDA_DISEQC_INIT,         // Custom property for Intializing Diseqc.
+		KSPROPERTY_BDA_SCAN_FREQ,           // Not supported 
+		KSPROPERTY_BDA_CHANNEL_CHANGE,      // Custom property for changing channel
+		KSPROPERTY_BDA_DEMOD_INFO,          // Custom property for returning demod FW state and version
+		KSPROPERTY_BDA_EFFECTIVE_FREQ,      // Not supported 
+		KSPROPERTY_BDA_SIGNAL_STATUS,       // Custom property for returning signal quality, strength, BER and other attributes
+		KSPROPERTY_BDA_LOCK_STATUS,         // Custom property for returning demod lock indicators 
+		KSPROPERTY_BDA_ERROR_CONTROL,       // Custom property for controlling error correction and BER window
+		KSPROPERTY_BDA_CHANNEL_INFO,        // Custom property for exposing the locked values of frequency,symbol rate etc after corrections and adjustments
 		KSPROPERTY_BDA_NBC_PARAMS,
 		KSPROPERTY_BDA_SETTSID = 96,		// added 20190704 for ISDB-S/S3 for BonBDA drivers
-	} KSPROPERTY_BDA_TUNER_EXTENSION;
+	};
 
 	/*******************************************************************************************************/
 	/* PHANTOM_LNB_BURST */
 	/*******************************************************************************************************/
-	typedef enum _PHANTOMLnbburst  {
-		PHANTOM_LNB_BURST_MODULATED=1,                /* Modulated: Tone B               */
-		PHANTOM_LNB_BURST_UNMODULATED,                /* Not modulated: Tone A           */
-		PHANTOM_LNB_BURST_UNDEF=0                     /* undefined (results in an error) */
-	}   PHANTOM_LNB_BURST;
+	enum PHANTOM_LNB_BURST : ULONG {
+		PHANTOM_LNB_BURST_UNDEF = 0,				// undefined (results in an error)
+		PHANTOM_LNB_BURST_MODULATED = 2,			// Modulated: Tone B
+		PHANTOM_LNB_BURST_UNMODULATED = 3,			// Not modulated: Tone A
+	};
 	/*******************************************************************************************************/
 	/* PHANTOM_RXMODE */
 	/*******************************************************************************************************/
-	typedef enum _PHANTOMRxMode  {
-		PHANTOM_RXMODE_INTERROGATION=0,              /* Demod expects multiple devices attached */
-		PHANTOM_RXMODE_QUICKREPLY=1,                 /* demod expects 1 rx (rx is suspended after 1st rx received) */
-		PHANTOM_RXMODE_NOREPLY=2                     /* demod expects to receive no Rx message(s) */
-	}   PHANTOM_RXMODE;
+	enum PHANTOM_RXMODE : ULONG {
+		PHANTOM_RXMODE_INTERROGATION = 0,			// Demod expects multiple devices attached
+		PHANTOM_RXMODE_QUICKREPLY = 1,				// demod expects 1 rx (rx is suspended after 1st rx received)
+		PHANTOM_RXMODE_NOREPLY = 2,					// demod expects to receive no Rx message(s)
+	};
 
+	enum TBSDVBSExtensionPropertiesCMDMode : ULONG {
+		TBSDVBSCMD_LNBPOWER = 0x00,
+		TBSDVBSCMD_MOTOR = 0x01,
+		TBSDVBSCMD_22KTONEDATA = 0x02,
+		TBSDVBSCMD_DISEQC = 0x03,
+	};
+
+	enum HZ_22K_TONE : BYTE {
+		HZ_22K_OFF = 0,
+		HZ_22K_ON = 1,
+	};
+
+	enum TONE_DATA_BURST : UCHAR {
+		Tone_Burst_ON = 0,
+		Data_Burst_ON = 1,
+		Tone_Data_Disable = 2,
+	};
+
+	enum LNB_POWER : BOOL {
+		LNB_POWER_OFF = FALSE,
+		LNB_POWER_ON = TRUE,
+	};
 
 	// DVB-S/S2 DiSEqC message parameters
-	typedef struct _DISEQC_MESSAGE_PARAMS
+	struct DISEQC_MESSAGE_PARAMS
 	{
-		UCHAR      uc_diseqc_send_message[DISEQC_TX_BUFFER_SIZE+1];
-		UCHAR      uc_diseqc_send_message_length;
-		UCHAR      uc_diseqc_receive_message[DISEQC_RX_BUFFER_SIZE+1];
-		UCHAR      uc_diseqc_receive_message_length;    
+		UCHAR uc_diseqc_send_message[DISEQC_TX_BUFFER_SIZE + 1];
+		UCHAR uc_diseqc_send_message_length;
+		UCHAR uc_diseqc_receive_message[DISEQC_RX_BUFFER_SIZE + 1];
+		UCHAR uc_diseqc_receive_message_length;
 
-		PHANTOM_LNB_BURST burst_tone;       //Burst tone at last-message: (modulated = ToneB; Un-modulated = ToneA). 
-		PHANTOM_RXMODE    receive_mode;     //Reply mode: interrogation/no reply/quick reply.
-		TBSDVBSExtensionPropertiesCMDMode tbscmd_mode;
+		ULONG burst_tone;			// Burst tone at last-message: (modulated = ToneB; Un-modulated = ToneA). 
+		ULONG receive_mode;			// Reply mode: interrogation/no reply/quick reply.
+		ULONG tbscmd_mode;
 
-		UCHAR           HZ_22K;                 // HZ_22K_OFF | HZ_22K_ON
-		UCHAR           Tone_Data_Burst;        // Data_Burst_ON | Tone_Burst_ON |Tone_Data_Disable
-		UCHAR      uc_parity_errors;        //Parity errors:  0 indicates no errors; binary 1 indicates an error.
-		UCHAR      uc_reply_errors;         //1 in bit i indicates error in byte i. 
-		BOOL       b_last_message;          //Indicates if current message is the last message (TRUE means last message).
-		BOOL       b_LNBPower;//liuzheng added for lnb power static
-
-	} DISEQC_MESSAGE_PARAMS, *PDISEQC_MESSAGE_PARAMS;
-
+		UCHAR HZ_22K;				// HZ_22K_OFF | HZ_22K_ON
+		UCHAR Tone_Data_Burst;		// Data_Burst_ON | Tone_Burst_ON |Tone_Data_Disable
+		UCHAR uc_parity_errors;		// Parity errors:  0 indicates no errors; binary 1 indicates an error.
+		UCHAR uc_reply_errors;		// 1 in bit i indicates error in byte i. 
+		BOOL b_last_message;		// Indicates if current message is the last message (TRUE means last message).
+		BOOL b_LNBPower;			// liuzheng added for lnb power static
+	};
 };
